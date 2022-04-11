e41f321dd134c240d24fb11ecb1f395e65f80b6b
pogo <birsdrinker@gmail.com>
2022-04-11 17:34:32 +0200

    vingenere fail so far

---
+++ vigenere.py
@@ -0,0 +1,25 @@
+#def vig(cltxt, key):
+
+txt = "picoctf"
+key = "hello"
+
+tabel_txt = []
+tabel_key = []
+
+for i in txt:
+ tabel_txt.append(ord(i))
+
+ for j in key:
+  tabel_key.append(ord(i))
+
+    
+        
+print(tabel_txt)
+print(tabel_key)
+
+
+
+
+
+
+
